98330f81334f2f7e7cc9b3db839de6a34261ada8
Antonio Jesús <antoniojesuspv99@gmail.com>
2023-04-10 00:54:25 +0200

    creación subcripción del seguimiento Implementación bloc para registrar el seguimiento en la lista

--- lib/blocs/location/location_bloc.dart
+++ lib/blocs/location/location_bloc.dart
@@ -1,11 +1,46 @@
+import 'dart:async';
+
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart' show LatLng;
 
 part 'location_event.dart';
 part 'location_state.dart';
 
 class LocationBloc extends Bloc<LocationEvent, LocationState> {
+  StreamSubscription? positionStream;
+
   LocationBloc() : super(const LocationState()) {
-    on<LocationEvent>((event, emit) {});
+    on<OnNewUserLocationEvent>((event, emit) {
+      emit(state.copywith(
+          lastKnownLocation: event.newLocation,
+          myLocationHistory: [...state.myLocationHistory, event.newLocation]));
+    });
+  }
+
+  Future getCurrentPosition() async {
+    final position = await Geolocator.getCurrentPosition();
+    print('position -> $position');
+  }
+
+  void startFollowingUser() {
+    print('startFollowingUser');
+    positionStream = Geolocator.getPositionStream().listen((event) {
+      final position = event;
+      add(OnNewUserLocationEvent(
+          LatLng(position.latitude, position.longitude)));
+    });
+  }
+
+  void stopFollowingUser() {
+    positionStream?.cancel();
+    print('stopFollowingUser');
+  }
+
+  @override
+  Future<void> close() {
+    positionStream?.cancel();
+    return super.close();
   }
 }
